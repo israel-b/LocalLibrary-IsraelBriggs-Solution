5da1b7a5bfb69d90ce5126cd704b3ae4f0d84aaa
israel-b <briggs.israel@gmail.com>
2023-10-29 23:56:07 -0700

    Complete qualified assignment, ...

---
+++ public/src/books.js
@@ -0,0 +1,57 @@
+function findAuthorById(authors, id) {
+  for (const author of authors) {
+    if (author.id === id) {
+      return author;
+    }
+  }
+  return null;
+
+}
+
+function findBookById(books, id) {
+  for (const book of books) {
+    if (book.id === id) {
+      return book;
+    }
+  }
+  return null;
+}
+
+// NOTE: YOU DON'T HAVE TO EDIT THE FUNCTIONS BELOW
+function partitionBooksByBorrowedStatus(books) {
+  return books.reduce(
+    (acc, book) => {
+      const [borrowed, returned] = acc;
+      const recent = book.borrows[0];
+      if (recent.returned) {
+        returned.push(book);
+      } else {
+        borrowed.push(book);
+      }
+
+      return acc;
+    },
+    [[], []]
+  );
+}
+
+function getBorrowersForBook(book, accounts) {
+  const accountsById = accounts.reduce((acc, account) => {
+    acc[account.id] = account;
+    return acc;
+  }, {});
+
+  return book.borrows
+    .map(({ id, returned }) => ({
+      ...accountsById[id],
+      returned,
+    }))
+    .slice(0, 10);
+}
+
+module.exports = {
+  findAuthorById,
+  findBookById,
+  partitionBooksByBorrowedStatus,
+  getBorrowersForBook,
+};
